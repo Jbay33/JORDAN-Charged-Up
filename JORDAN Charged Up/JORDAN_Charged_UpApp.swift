7f7a3ecfc8083f7248c28f5b5bb72f8a70b52ea6
Justin Baylis <jbaylis01@haverfordsd.net>
2023-01-09 13:46:59 -0500

    Initial Commit

---
+++ JORDAN Charged Up/JORDAN_Charged_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  JORDAN_Charged_UpApp.swift
+//  JORDAN Charged Up
+//
+//  Created by  on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct JORDAN_Charged_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
