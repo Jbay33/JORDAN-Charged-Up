1736788d2b0f3932817f299e624b535345c711b3
Justin Baylis <jbaylis01@haverfordsd.net>
2023-01-09 14:02:37 -0500

    implode, physical

--- JORDAN Charged Up/ContentView.swift
+++ JORDAN Charged Up/ContentView.swift
@@ -6,21 +6,7 @@
 //
 
 import SwiftUI
-
+import SpriteKit
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   
 }
---
+++ JORDAN Charged Up/TeamView.swift
@@ -0,0 +1,20 @@
+//
+//  TeamView.swift
+//  JORDAN Charged Up
+//
+//  Created by  on 1/9/23.
+//
+
+import SwiftUI
+
+struct TeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TeamView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamView()
+    }
+}
