328372b7b27e87a1543663abdb7ef973be6b98ac
sdxix <milow2u@gmail.com>
2023-01-19 14:17:06 -0500

    It almost works

--- JORDAN Charged Up/FieldLayout.swift
+++ JORDAN Charged Up/FieldLayout.swift
@@ -9,16 +9,122 @@
 import Foundation
 import SpriteKit
 
-    class FieldLayout : SKScene{
-        var start = SKLabelNode()
+class FieldLayout : SKScene{
+    var pieces = [
+        //MARK: sect a
+        [SKSpriteNode()],
+        [SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        
+        [SKSpriteNode(), SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        
+        [SKSpriteNode()],
+        [SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        
+        
+        //MARK: sect B
+        [SKSpriteNode()],
+        [SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        
+        [SKSpriteNode(), SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        
+        [SKSpriteNode()],
+        [SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        
+        
+        //MARK: sect C
+        [SKSpriteNode()],
+        [SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        
+        [SKSpriteNode(), SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()],
+        
+        [SKSpriteNode()],
+        [SKSpriteNode()],
+        [SKSpriteNode(), SKSpriteNode()]
+    ]
+    
+    var sectA = SKNode()
+    var sectB = SKNode()
+    var sectC = SKNode()
 
-        override func didMove(to view: SKView) {
-            //start = childNode(withName: "Start") as! SKLabelNode
+    override func didMove(to view: SKView) {
+        sectA = childNode(withName: "Peices")!.childNode(withName: "SectA")!
+        sectB = childNode(withName: "Peices")!.childNode(withName: "SectB")!
+        sectC = childNode(withName: "Peices")!.childNode(withName: "SectC")!
+        
+        for i in 0..<9 {
+            if pieces[i].count > 1 {
+                pieces[i][0] = sectA.childNode(withName: "\(i+1)Cone") as! SKSpriteNode
+                pieces[i][1] = sectA.childNode(withName: "\(i+1)Cube") as! SKSpriteNode
+            } else {
+                pieces[i][0] = sectA.childNode(withName: "\(i+1)") as! SKSpriteNode
+            }
         }
         
-        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-            let location = touches.first!.location(in: self)
+        for i in 9..<18 {
+            if pieces[i].count > 1 {
+                pieces[i][0] = sectB.childNode(withName: "\(i-8)Cone") as! SKSpriteNode
+                pieces[i][1] = sectB.childNode(withName: "\(i-8)Cube") as! SKSpriteNode
+            } else {
+                pieces[i][0] = sectB.childNode(withName: "\(i-8)") as! SKSpriteNode
+            }
+        }
+        
+        for i in 18..<27 {
+            if pieces[i].count > 1 {
+                pieces[i][0] = sectC.childNode(withName: "\(i-17)Cone") as! SKSpriteNode
+                pieces[i][1] = sectC.childNode(withName: "\(i-17)Cube") as! SKSpriteNode
+            } else {
+                pieces[i][0] = sectC.childNode(withName: "\(i-17)") as! SKSpriteNode
+            }
+        }
+        
+        for i in pieces {
+            print(i[0].frame)
+        }
+        
+    }
+    
+    //ugly please fix please
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: self)
+        
+        for i in pieces {
+            if i.count > 1 {
+                if !i[0].isHidden && i[0].frame.contains(location)  {
+                    i[0].isHidden = true
+                    i[1].isHidden = false
+                    break
+                } else if !i[1].isHidden && i[0].frame.contains(location) {
+                    i[1].isHidden = true
+                    i[0].isHidden = true
+                    break
+                } else if i[0].frame.contains(location) {
+                    i[0].isHidden = false
+                    i[1].isHidden = true
+                    break
+                }
+            } else {
+                if !i[0].isHidden && i[0].frame.contains(location) {
+                    i[0].isHidden = true
+                    break
+                } else if i[0].frame.contains(location) {
+                    i[0].isHidden = false
+                    break
+                }
+            }
         }
     }
+}
 
 
